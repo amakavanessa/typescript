cf0da97f22e2f5e62e880bdee44a379a1bc6335e
Precious Nnam <nnamvannessa@gmail.com>
2023-06-12 16:30:38 +0100

    notes on using third party js libraries with typescript

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import { ProjectInput } from "./components/project-input";
-import { ProjectList } from "./components/project-list";
-new ProjectInput();
-new ProjectList("active");
-new ProjectList("completed");
+import _ from "lodash";
+//there are translation types packages they usually come with .dts they exist for popular third party libraries, they translate the js version of the code to its ts equivalent. search for the library name and include types
+console.log(_.shuffle([1, 2, 3, 4, 5, 6]));
+
+// when you have a library that doesnt have types or js you include in a script tag in your html code. you use 'declare' keyword and include the name and its output
