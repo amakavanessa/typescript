57fca0d26c208697f81946245e6f896229f78ea6
Precious Nnam <nnamvannessa@gmail.com>
2023-06-12 18:58:22 +0100

    notes on using third party js libraries with typescript,(Class-Validators  and using third party decorators)

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 //this is used for converting plain objects like json to class objects
 import { plainToInstance } from "class-transformer";
-
+import { validate } from "class-validator";
 import { Product } from "./product.model";
 
 const products = [
@@ -15,6 +15,16 @@ const products = [
   { title: "drum", price: "10" },
   { title: "purse", price: "80" },
 ];
+
+const newProd = new Product("", -50);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log("VALIDATION ERRORS!");
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
 
 const loadedProducts = plainToInstance(Product, products);
 for (const prod of loadedProducts) {
--- src/product.model.ts
+++ src/product.model.ts
@@ -1,5 +1,10 @@
+import { IsNotEmpty, IsNumber, IsPositive } from "class-validator";
+// always remember to turn experimentalDecorators to true on tsconfig when you want to use decorators
 export class Product {
+  @IsNotEmpty()
   title: string;
+  @IsNumber()
+  @IsPositive()
   price: number;
 
   constructor(t: string, p: number) {
