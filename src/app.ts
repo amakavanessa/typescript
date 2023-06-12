d51341f3fca89fe22a2769ad81f6f9db311e5905
Precious Nnam <nnamvannessa@gmail.com>
2023-06-12 18:36:45 +0100

    using class-transformer 3rd party library to transform from json object to classes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,22 @@
-import _ from "lodash";
-//there are translation types packages they usually come with .dts they exist for popular third party libraries, they translate the js version of the code to its ts equivalent. search for the library name and include types
-console.log(_.shuffle([1, 2, 3, 4, 5, 6]));
+import "reflect-metadata";
+//this is used for converting plain objects like json to class objects
+import { plainToInstance } from "class-transformer";
 
-// when you have a library that doesnt have types or js you include in a script tag in your html code. you use 'declare' keyword and include the name and its output
+import { Product } from "./product.model";
+
+const products = [
+  { title: "hair", price: "100" },
+  { title: "shoe", price: "50" },
+  { title: "top", price: "20" },
+  { title: "bag", price: "100" },
+  { title: "food", price: "70" },
+  { title: "laptop", price: "500" },
+  { title: "phone", price: "800" },
+  { title: "drum", price: "10" },
+  { title: "purse", price: "80" },
+];
+
+const loadedProducts = plainToInstance(Product, products);
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
