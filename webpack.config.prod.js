7b9216744a2a06d7ffb6fd49926c6b3a93146adb
Precious Nnam <nnamvannessa@gmail.com>
2023-06-12 13:08:26 +0100

    modularized code using es6 modules and optimization using webpack(production work flow)

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,8 +1,10 @@
 const path = require("path");
+// This cleans up the dist folder after the project is successfully built
+const CleanPlugin = require("clean-webpack-plugin");
 // //webpack is a bundling tool, it helps reduce http request by bundling codes together,less imports required,  it optimizes the codes by minifying it.
 //it is also able to transform ts to js codes and bundle it
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/app.ts",
   devServer: {
     static: [
@@ -14,14 +16,13 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/",
   },
-  devtool: "inline-source-map",
+
   module: {
     rules: [
       {
         //a regular expression that checks for the file ext
-        test: /\.tsx?$/,
+        test: /\.ts$/,
         use: "ts-loader",
         exclude: /node_modules/,
       },
@@ -30,4 +31,6 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".js"],
   },
+
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
 };
